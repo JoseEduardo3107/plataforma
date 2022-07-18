93b0a9c5c000e19d67fb112a16a0da206d53c056
JoseEduardo3107 <josseduardo7u7@gmail.com>
2022-07-18 18:32:51 -0500

    desktop rules (notifications)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-
 function switchInputPasswordMode(){
     let buttonSwitch = document.getElementById('buttonSwitchInput');
     let inputPassword = document.getElementById('inputPassword');
-
-    //buttonSwitch.value = "Ocultar";
 
     if(inputPassword.type == "password"){
         inputPassword.type = "text";
@@ -14,8 +11,10 @@ function switchInputPasswordMode(){
     }
 }
 
+// Queue
 const containerNotifications = document.getElementById("containerNotifications");
 
+// Class for notifications
 class createToastMessage{
     constructor(tabTitle, tabMessage, tabColor, displayTime = null, tabLink=null, tabLinkMessage=null){
 
@@ -26,8 +25,8 @@ class createToastMessage{
         this.tabLink = tabLink;
         this.tabLinkMessage = tabLinkMessage;
 
-          // Show tab (Message)
-          this.showTab();
+        // Show tab (Message)
+        this.showTab();
 
         // Add timer if exists
         if(this.displayTime = displayTime){
@@ -61,64 +60,62 @@ class createToastMessage{
 
     // Create Tab
     showTab(){
-        // Create tab
-
-        // estructura de pestaña general
+        // Layout 1
         this.notificationTab = document.createElement('div');
         this.notificationTab.classList.add('notificationTab');
         
-            // estructura de titulo
-            this.notificationTabTitle = document.createElement('div');
-            this.notificationTabTitle.classList.add('blockLayoutTab');
-            this.notificationTabTitle.classList.add('tabTitle');
-            this.notificationTabTitle.innerHTML = this.tabTitle;
+        // Layout 2
+        this.notificationTabTitle = document.createElement('div');
+        this.notificationTabTitle.classList.add('blockLayoutTab');
+        this.notificationTabTitle.classList.add('tabTitle');
+        this.notificationTabTitle.innerHTML = this.tabTitle;
 
-            // estructura de mensaje
-            this.notificationTabText = document.createElement('div');
-            this.notificationTabText.classList.add('blockLayoutTab');
-            this.notificationTabText.innerHTML = this.tabMessage;
+        // Layout 3
+        this.notificationTabText = document.createElement('div');
+        this.notificationTabText.classList.add('blockLayoutTab');
+        this.notificationTabText.innerHTML = this.tabMessage;
 
-            // estructura de cronometro
-            this.notificationTabTimer = document.createElement('div');
-            this.notificationTabTimer.classList.add('blockLayoutTab');
+        // Layout 4
+        this.notificationTabTimer = document.createElement('div');
+        this.notificationTabTimer.classList.add('blockLayoutTab');
 
-            // cronometro (visual)
-            this.notificationTimer = document.createElement('span');
-            this.notificationTimer.style.background = this.tabColor;
-            this.notificationTimer.classList.add('timerBar');
+        // Layout 4 (visual)
+        this.notificationTimer = document.createElement('span');
+        this.notificationTimer.style.background = this.tabColor;
+        this.notificationTimer.classList.add('timerBar');
 
-            // Join elements
-            // Agrega cronometro visual a estructura de cronometro
-            this.notificationTabTimer.appendChild(this.notificationTimer);
+        // Join elements
+        // Add Layout 4 (visual) to Layout 4
+        this.notificationTabTimer.appendChild(this.notificationTimer);
 
+        // Add if exists link
+        if(this.tabLink){
+            // Layout 5 (Link)
+            this.notificationTextA = document.createElement('a');
+            this.notificationTextA.setAttribute("href", this.tabLink);
+            this.notificationTextA.innerHTML = " " + this.tabLinkMessage;
 
-            if(this.tabLink){
-                // configura el vinculo
-                this.notificationTextA = document.createElement('a');
-                this.notificationTextA.setAttribute("href", this.tabLink);
-                this.notificationTextA.innerHTML = " " + this.tabLinkMessage;
+            // Add Layout 5 to Layout 3
+            this.notificationTabText.appendChild(this.notificationTextA);
+        }
 
-                // inserta link
-                this.notificationTabText.appendChild(this.notificationTextA);
-            }
+        // Add Layout 2 to Layout 1
+        this.notificationTab.appendChild(this.notificationTabTitle);
 
-            // Agrega estructura de titulo a estructura de pestaña general
-            this.notificationTab.appendChild(this.notificationTabTitle);
+        // Add Layout 3 to Layout 1
+        this.notificationTab.appendChild(this.notificationTabText);
 
-            //Agrega estructura de mensaje a estructura de pestaña general
-            this.notificationTab.appendChild(this.notificationTabText);
+        // Add Layout 4 a Layout 1
+        this.notificationTab.appendChild(this.notificationTabTimer);
 
-            // Agrega estructura de cronometro a estructura de pestaña general
-            this.notificationTab.appendChild(this.notificationTabTimer);
+        // Add Layout 1 to queue
+        containerNotifications.appendChild(this.notificationTab);
 
-            // agregar estructura de pestaña general a la fila
-            containerNotifications.appendChild(this.notificationTab);
+        // Paint (element) on queue
+        this.notificationTab.clientHeight;
 
-            // Paint (element) - volver a pintar elemento para que la animacion ocurra
-            this.notificationTab.clientHeight;
-
-            //Add additional animation
-            this.notificationTab.classList.add('containerNotificationTab-initialAnimation');
+        //Add additional animation
+        this.notificationTab.classList.add('containerNotificationTab-initialAnimation');
     }
 
     // Timer
@@ -145,7 +142,6 @@ class createToastMessage{
             clearInterval(this.displayTimeCounter);
         }
 
-
         // Add class to animation
         this.notificationTab.classList.add('cleanTab');
         this.notificationTabText.classList.add('cleanTab-Text');
@@ -156,13 +152,11 @@ class createToastMessage{
         // Time out (Animation)
         this.notificationTab.remove();
 
-
         delete this.createToastMessage;
     }
-
 }
 
+// create new toast message (title, message, timer bar color [normal, hexadecimal, rgba], optional - time on screen [milliseconds], link [href], link [title])
 function newToastMessage(tabTitle, tabMessage, tabColor, displayTime = null, tabLink=null, tabLinkMessage=null){
     let newMessage = new createToastMessage(tabTitle, tabMessage, tabColor, displayTime, tabLink, tabLinkMessage);
 }
-
